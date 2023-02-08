edee886e875ee677c468b7958438166accb0f805
huixiangcun <119782524+huixiangcun@users.noreply.github.com>
2023-02-08 19:24:23 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,22 @@
+//% color="#006400" weight=20 icon="\uf1b9"
+namespace cbit_小车类 {
+    //% blockId=cbit_ultrasonic_car block="超声波返回(cm)|Trig %pin1|Echo %pin2"
+    //% color="#006400"
+    //% weight=98
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Ultrasonic_Car(pin1: DigitalPin, pin2: DigitalPin): number {
+
+        // send pulse
+        pins.setPull(pin1, PinPullMode.PullNone);
+        pins.digitalWritePin(pin1, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(pin1, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(pin1, 0);
+
+        // read pulse
+        let d = pins.pulseIn(pin2, PulseValue.High, 43200);
+        return d / 58;
+    }
+}
