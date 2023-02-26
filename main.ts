694d40a0af9436254f12922030d099751ae6f8c2
huixiangcun <119782524+huixiangcun@users.noreply.github.com>
2023-02-26 11:58:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,13 +7,13 @@ namespace 小车类 {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Ultrasonic_Car(pin1: DigitalPin, pin2: DigitalPin): number {
 
-        // send pulse
+        
         pins.setPull(pin1, PinPullMode.PullNone);
         pins.digitalWritePin(pin1, 0);
         control.waitMicros(2);
         pins.digitalWritePin(pin1, 1);
         control.waitMicros(10);
-        pins.digitalWritePin(pin1, 0);
+        pins.digitalWritePin(pin1,0);
 
         // read pulse
         let d = pins.pulseIn(pin2, PulseValue.High, 43200);
